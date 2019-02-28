5161bbd879f840e76b96cfaeed4c8c9caadbfcb3
Vadim <testov@test.ua>
2019-02-28 21:59:52 +0200

    .

--- Course2Lesson9Task3Reader/src/ua/testov/test/RepeatLetter.java
+++ Course2Lesson9Task3Reader/src/ua/testov/test/RepeatLetter.java
@@ -2,24 +2,37 @@ package ua.testov.test;
 
 import java.util.Comparator;
 
-public class RepeatLetter implements Comparable<RepeatLetter>{
-private char x;
-private int y;
-public RepeatLetter() {
-	super();
-}
-public RepeatLetter(char x) {
-	super();
-	this.x = x;
-}
+public class RepeatLetter implements Comparable<RepeatLetter> {
+	private char x;
+	private int y = 0;
 
-@Override
-public int compareTo(RepeatLetter o) {
-	if(this.y>o.y) {
-		return -1;
-	}else if(this.y<o.y) {
-		return 1;
-	}else return 0;
-}
+	public RepeatLetter() {
+		super();
+	}
+
+	public RepeatLetter(char x) {
+		super();
+		this.x = x;
+	}
+
+	public int countSymbols(String text) {
+		char[] array = text.toCharArray();
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == x) {
+				y = y + 1;
+			}
+		}
+		return y;
+	}
+
+	@Override
+	public int compareTo(RepeatLetter o) {
+		if (this.y > o.y) {
+			return -1;
+		} else if (this.y < o.y) {
+			return 1;
+		} else
+			return 0;
+	}
 
 }
